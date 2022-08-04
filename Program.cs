0e2e4eafbb354d1c7ed59e3fa4c9313d375a6a96
swathimathalam <email>
2022-08-04 17:16:57 +0530

    First comit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Son : Father
+{
+    public void DisplayTwo()
+    {
+        Console.WriteLine("Son.. ");
+    }
+    static void Main(string[] args)
+    {
+        Son s = new Son();
+        s.Display();
+        s.DisplayOne();
+        s.DisplayTwo();
+        Console.Read();
+    }
+}
+class Grandfather
+{
+    public void Display()
+    {
+        Console.WriteLine("Grandfather...");
+    }
+}
+class Father : Grandfather
+{
+    public void DisplayOne()
+    {
+        Console.WriteLine("Father...");
+    }
+}
